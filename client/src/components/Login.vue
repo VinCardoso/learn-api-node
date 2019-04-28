d5041671b697f5b75c02d0217256222511f13f2d
Vinicius Cardoso <viniciuscc10@gmail.com>
2019-04-28 20:58:04 -0300

    first commit

---
+++ client/src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+
+      <v-flex xs12>
+        <h2>Login</h2>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-text-field
+        label="E-mail"
+        v-model="email"
+        v-bind:rules="emailRules"
+        required>
+        </v-text-field>
+
+        <v-text-field
+        label="Senha"
+        v-model="password"
+        v-bind:rules="passwordRules"
+        v-bind:type="'password'"
+        requered
+        >
+        </v-text-field>
+      </v-flex>
+
+      <v-flex xs12 sm6 offset-sm3>
+        <v-btn v-on:click="cancel">Cancel</v-btn>
+        <v-btn color="primary" v-on:click="login">Login</v-btn>
+      </v-flex>
+
+      <v-snackbar
+        :timeout="6000"
+        :top="true"
+        v-model="showAlert"
+      >
+      {{ messege }}
+      </v-snackbar>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showAlert: false,
+      messege: '',
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail é obrigatório',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Este não parece um e-mail válido'
+      ],
+      password: '',
+      passwordRules: [v => !!v || 'Você precisa digitar sua senha']
+    }
+  },
+  methods: {
+    login: function () {
+      const vm = this
+      if (vm.password === '10203045') {
+        this.$router.push({ path: '/' })
+      } else {
+        vm.showAlert = true
+        vm.messege = 'Login ou Senha Inválidos'
+      }
+    },
+    cancel: function () {
+      console.log('O usuário não quer se conectar')
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const api = require('./api');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.set('port', (process.env.PORT || 8081));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', api);
+app.use(express.static('static'));
+
+app.use(morgan('dev'));
+
+app.use(function (req, res, next){
+    const err = new Error('Not Found');
+    err.status = 404;
+    res.json(err);
+});
+
+mongoose.connect(
+  'mongodb://vinicius:02TM40XGeJCp@ds143241.mlab.com:43241/globalmantics',
+  { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+
+  app.listen(app.get('port'), () => {
+    console.log('API Server Listening on port ' + app.get('port'));
+  });
+});
